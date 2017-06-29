11a5aa4e7645e1a707bbf90dea1ca21a71e3449f
if1live <libsora25@gmail.com>
2017-06-29 23:19:34 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,57 @@
+var fired = false;
+var watcher = null;
+var result = null;
+
+function watchTrigger() {
+  if (fired) {
+    return;
+  }
+
+  try {
+    if (!fired) {
+      result = trigger();
+      console.log(result);
+      fired = true;
+
+      updateMessage("Ready");
+      document.querySelector('#btn-trigger').disabled = false;
+
+      if(watcher) {
+        clearTimeout(watcher);
+      }
+    }
+  } catch (e) {
+    console.log("wait bullet loading...: " + e);
+    watcher = setTimeout(watchTrigger, 10);
+  }
+}
+
+document.querySelector('#btn-trigger').disabled = true;
+watchTrigger();
+
+function updateMessage(text) {
+  var el = document.querySelector('#trigger-result');
+  el.innerHTML = text;
+}
+
+document.querySelector('#btn-trigger').onclick = function() {
+  if(!result) {
+    return;
+  }
+
+  this.disabled = true;
+
+  var msg = '';
+  msg += `slot ${result.num} triggered.<br/>`;
+  if(result.fired) {
+    msg += "<strong>Bang.</strong>"
+  } else {
+    msg += "<strong>click.</strong>";
+  }
+  updateMessage(msg);
+
+}
+
+function runTrigger(val) {
+
+}
---
+++ slot_gen.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+
+function buildSlotSource(num, target) {
+  return `
+// generated - start
+console.log('slot ${num} loaded.')
+
+setTimeout(function() {
+  function trigger() {
+    return {
+      fired: (${target} == ${num}),
+      num: ${num}
+    };
+  }
+  window.trigger = trigger;
+  console.log('slot ${num} registered.')
+}, 3000);
+// generated - end
+  `;
+}
+
+var dummy = fs.readFileSync('jquery-3.2.1.min.js', 'utf8');
+
+var total = 6;
+var target = 5;
+for (var i = 1; i <= total; i++) {
+  var src = buildSlotSource(i, target);
+  var full = dummy + '\n' + src;
+  var filename = `slot-${i}.js`
+  fs.writeFile(filename, full, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+}
